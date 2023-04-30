8d53edeed167c6a0ee896f1d3f08062a6b0a31c4
aayafrontenda <eeaayeyeah@gmail.com>
2023-04-30 16:17:25 +0600

    - division by 0 error is handled

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,74 @@ export const ACTIONS = {
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear', 
   DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate'
+  EVALUATE: 'evaluate',
+}
+
+// sum of floating point numbers
+function add(a, b) {
+  var c, d, e;
+  try {
+      c = a.toString().split(".")[1].length;
+  } catch (f) {
+      c = 0;
+  }
+  try {
+      d = b.toString().split(".")[1].length;
+  } catch (f) {
+      d = 0;
+  }
+  return e = Math.pow(10, Math.max(c, d)), (mul(a, e) + mul(b, e)) / e;
+}
+
+// Subtract floating point numbers
+function sub(a, b) {
+  var c, d, e;
+  try {
+      c = a.toString().split(".")[1].length;
+  } catch (f) {
+      c = 0;
+  }
+  try {
+      d = b.toString().split(".")[1].length;
+  } catch (f) {
+      d = 0;
+  }
+  return e = Math.pow(10, Math.max(c, d)), (mul(a, e) - mul(b, e)) / e;
+}
+
+// Multiply floating point numbers
+function mul(a, b) {
+  var c = 0,
+      d = a.toString(),
+      e = b.toString();
+  try {
+      c += d.split(".")[1].length;
+  } catch (f) {}
+  try {
+      c += e.split(".")[1].length;
+  } catch (f) {}
+  return Number(d.replace(".", "")) * Number(e.replace(".", "")) / Math.pow(10, c);
+}
+
+// divide floating point
+function div(a, b) {
+  var c, d, e = 0,
+      f = 0;
+  try {
+      e = a.toString().split(".")[1].length;
+  } catch (g) {}
+  try {
+      f = b.toString().split(".")[1].length;
+  } catch (g) {}
+  return c = Number(a.toString().replace(".", "")), d = Number(b.toString().replace(".", "")), mul(c / d, Math.pow(10, f - e));
 }
 
 function reducer(state, { type, payload }) {
+  if (state.currentOperand === '{error}')
+    return {};
+
   switch (type) {
-    case ACTIONS.ADD_DIGIT:
+    case ACTIONS.ADD_DIGIT:      
       if (state.isEvaluated) {
         return {
           // ...state, currentOperand: currentOperand + payload.digit
@@ -29,7 +91,7 @@ function reducer(state, { type, payload }) {
       if (payload.digit === '0' && state.currentOperand === '0')
         return state;
 
-      if (payload.digit === '.' && state.currentOperand.toString().includes('.'))
+      if (payload.digit === '.' && state.currentOperand !== null && state.currentOperand.toString().includes('.'))
         return state;
       
       return {
@@ -123,16 +185,20 @@ function evaluate({ currentOperand, previousOperand, operation }) {
 
   switch(operation) {
     case '+':
-      return prev + curr;
+      return add(prev, curr);
 
     case '-':
-      return prev - curr;
+      return sub(prev, curr);
 
     case '÷':
-      return prev / curr;
+      if (curr == 0) {
+        return '{error}';
+      }
+       
+      return div(prev, curr);
 
     case '*':
-      return prev * curr;
+      return mul(prev, curr);
   }
 }
 
@@ -143,7 +209,7 @@ function App() {
       <div className='calculator-grid'>
         <div className='output'>
           <div className='previous-operand'>{previousOperand}{operation}</div>
-          <div className='current-operand'>{formatOperand(currentOperand)}</div>
+          <div className='current-operand'>{(currentOperand !== '{error}') ? formatOperand(currentOperand) : currentOperand}</div>
         </div>
         <button onClick={() => dispatch({ type: ACTIONS.CLEAR })} className='span-two'>AC</button>
         <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
